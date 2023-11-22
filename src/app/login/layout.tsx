753abeab4373729eadcec4246ec033ceb6d289f0
XYi <xiangly203@outlook.com>
2023-11-22 22:10:16 +0800

    add Next UI

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -1,6 +1,6 @@
 // app/layout.tsx
 import {Providers} from "../providers";
-import {Analytics} from '@vercel/analytics/react';
+// import {Analytics} from '@vercel/analytics/react';
 export default function RootLayout({children}: { children: React.ReactNode }) {
     return (
         <html lang="en" className='dark'>
