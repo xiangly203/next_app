4c3deee6cf0f64353b02c2504c2557da011e5b7d
XYi <xiangly203@outlook.com>
2023-12-05 22:06:13 +0800

    add heart.jsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
-// "use client"
 import React from "react";
-import {auth} from "../../auth";
-
+import Post from "@/components/layout/post";
+import {Pagination} from "@nextui-org/pagination";
 
 export default function APP(){
-    // const session = await auth()
-    // const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
-    // @ts-ignore
     return (
-
-        <div>
-            <h1>Main page</h1>
-        </div>);
+        <div className="flex flex-col items-center justify-between min-h-screen p-8">
+            <section className="flex-grow flex items-center mb-auto">
+                <Post/>
+            </section>
+            <Pagination total={10} initialPage={1} className="mt-8"/>
+        </div>
+    );
 }
